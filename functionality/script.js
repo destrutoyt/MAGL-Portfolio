29004c79412251b579e1db505a14000a69b7a33b
Miguel Garces <miguelongriego24@gmail.com>
2024-10-20 20:43:44 -0400

    V3.0 - Bootstrap & JS Implementation. New Home Webpage. New dark mode file added.

--- functionality/script.js
+++ functionality/script.js
@@ -1,13 +1,44 @@
 const init = addEventListener("DOMContentLoaded", getUserPreference)
 const button = document.getElementById('theme_button');
+const moonImage = document.querySelector('.moon');
 const body = document.body;
 
 button.addEventListener('click', buttonClicked);
 
 function getUserPreference() {
     console.log("DOM loaded. getUserPreference() executed");
+
+    if (localStorage.getItem('theme') === 'dark') {
+        console.log('Dark theme detected');
+        darkMode();
+    }
+    else {
+        console.log("Light theme detected");
+        lightMode();
+    }
 }
 
 function buttonClicked() {
-    console.log('Button has been clicked');
+    if (body.classList.contains('darkMode')) {
+        console.log("body element contains .darkMode. Changing to Light Mode");
+        localStorage.setItem('theme', 'light');
+        lightMode();
+    }
+    else {
+        console.log("body element does NOT contains .darkMode. Changing to Dark Mode");
+        localStorage.setItem('theme', 'dark');
+        darkMode();
+    }
+}
+
+function lightMode() {
+    console.log("Light Mode On");
+    body.classList.remove('darkMode');
+    moonImage.src = './node_modules/bootstrap-icons/icons/lightbulb.svg';
+}
+
+function darkMode() {
+    console.log("Dark Mode On")
+    body.classList.add('darkMode');
+    moonImage.src = './node_modules/bootstrap-icons/icons/lightbulb-off.svg';
 }
